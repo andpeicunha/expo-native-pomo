91708bffe25b29cbf26fedb5f49912ecafbb2d6c
Peixoto <andre.peixoto@trin.ca>
2023-10-04 18:41:16 -0300

    config project

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
 import { styled } from 'nativewind';
+import React from 'react';
+import { Text, View } from 'react-native';
 
-const StyledSafeArea = styled(SafeAreaView);
+const StyledView = styled(View);
 const StyledText = styled(Text);
 
-export default function App() {
-    return (
-        <StyledSafeArea className='flex bg-[#fff] items-center justify-center'>
-            <StyledText className='text-red-600 text-4xl text-center p-5'>Expo With TypeScript and Tailwind CSS</StyledText>
-        </StyledSafeArea>
-    );
+function App() {
+  return (
+    <StyledView className="flex bg-[#fff] items-center justify-center">
+      <StyledText className="text-red-600 text-4xl text-center p-5">
+        T ypeScript and Tailwind CSS
+      </StyledText>
+    </StyledView>
+  );
 }
+
+export default App;
